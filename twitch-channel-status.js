5b3cfa310c9c4ef57392a3a489d5b7ca61af4648
Robisinho <roberto.tomas.cuentas@gmail.com>
2016-12-14 14:47:41 -0500

    debugging headers

--- twitch-channel-status.js
+++ twitch-channel-status.js
@@ -3,7 +3,7 @@
 
 // Create a closure with a reference to our script
 (function (document, $script) {
-  console.log('[twitch-channel-status] robbimu fork - v1.0.6')
+  console.log('[twitch-channel-status] robbimu fork - v1.0.7')
   // Allow customizing the script with various data-* attributes
   var clientid = $script.attr("data-clientid") || false
       attribute = $script.attr("data-attribute") || "data-twitch-channel",
@@ -46,12 +46,8 @@
     // Ask twitch for the status of all channels at once
     $.ajax({
       url: "https://api.twitch.tv/kraken/streams",
-/*      headers: {
+      headers: {
         'Client-ID': 'l01lduyl0cfxlgl45t85qiegau166q'
-      }, */
-      beforeSend: function (request)
-      {
-        request.setRequestHeader("Client-ID", clientid);
       },
       data: {"channel": Object.keys(channels).join(","), "limit": Object.keys(channels).length},
       cache: false,
